d793dc0c6f23fa798c3b4f7be2d391c302cfb1dd
skylock <emanuil.copil@gmail.com>
2020-12-09 15:11:15 +0200

    add sample courses

--- app.js
+++ app.js
@@ -2,13 +2,23 @@ const express = require('express');
 
 const app = express();
 
+
+const courses = [
+    { id: 1, name: "Node.js from A to Z" },
+    { id: 2, name: "Mastering C#" },
+    { id: 3, name: "Using typescript with Node.js" }
+];
+
 app.get('/', function (req, res) {
     res.send('Hello world');
 });
 
-app.get('/api/courses', function (req, res) {
-    const courses = [1, 2, 3];
+app.get('/api/courses/', function (req, res) {
     res.send(courses);
+});
+
+app.get('/api/courses/:id', function (req, res) {
+    res.send(courses[req.params.id]);
 });
 
 const port = process.env.APP_PORT || 3000;
